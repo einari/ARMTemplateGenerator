7b665479caf5440a1629592d09122df75aa6f653
Einar Ingebrigtsen <einar@dolittle.com>
2016-05-04 19:19:46 +0200

    Swapping main <-> app

--- Source/Source/app.js
+++ Source/Source/app.js
@@ -1,6 +1,20 @@
-import ko from "knockout";
-import bootstrap from "bootstrap";
- 
-import {index} from "./index";
+import express from "express";
+import {Catalog} from "./catalog.server";
+import compression from "compression";
 
-ko.applyBindings(new index());
+console.log("MAIN");
+
+var application = express();
+application.use(compression());
+application.use(express.static(__dirname+"/"));
+application.use(require("connect-livereload")());
+
+Catalog.initialize(application);
+
+var port = process.env.PORT || 3000;
+
+console.log("Listen to traffic on : "+port);
+
+application.listen(port, () => {
+    console.log("Running on port "+port);
+});
--- Source/Source/main.js
+++ Source/Source/main.js
@@ -1,20 +1,6 @@
-import express from "express";
-import {Catalog} from "./catalog.server";
-import compression from "compression";
+import ko from "knockout";
+import bootstrap from "bootstrap";
+ 
+import {index} from "./index";
 
-console.log("MAIN");
-
-var application = express();
-application.use(compression());
-application.use(express.static(__dirname+"/"));
-application.use(require("connect-livereload")());
-
-Catalog.initialize(application);
-
-var port = process.env.PORT || 3000;
-
-console.log("Listen to traffic on : "+port);
-
-application.listen(port, () => {
-    console.log("Running on port "+port);
-});
+ko.applyBindings(new index());
