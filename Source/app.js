f9533a57bff0644a0246305160badfcbbbd17d50
Einar Ingebrigtsen <einar@dolittle.com>
2016-05-05 00:18:37 +0200

    Here we go again

--- Source/app.js
+++ Source/app.js
@@ -8,6 +8,13 @@ app.use(express.static(__dirname+"/public"));
 
 Catalog.Catalog.initialize(app);
 
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
+
 module.exports = app;
 
 /*
