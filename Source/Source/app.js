88bd4d05d9c34cd41cb65f8b59230d99f4d8abcc
Einar Ingebrigtsen <einar@dolittle.com>
2016-05-04 20:21:38 +0200

    Disabling one of the listeners - can't have two..

--- Source/Source/app.js
+++ Source/Source/app.js
@@ -11,12 +11,13 @@ application.use(require("connect-livereload")());
 
 Catalog.initialize(application);
 
+/*
 var port = process.env.PORT || 3000;
 
 console.log("Listen to traffic on : "+port);
 
 application.listen(port, () => {
     console.log("Running on port "+port);
-});
+});*/
 
 module.exports = application;
