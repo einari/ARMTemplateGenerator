7a7e28b0974acb4eedf8684036fbc55daa7a1f3d
Einar Ingebrigtsen <einar@dolittle.com>
2016-05-06 08:04:43 +0200

    Putting everything in temp_data

--- Source/Source/TemplateGenerator.server.js
+++ Source/Source/TemplateGenerator.server.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const templatesFolder = "./public/generatedtemplates";
+const templatesFolder = "./temp_data";
 
 let getFileNameFor = (template) => {
     let fileName = `${templatesFolder}/${template.id}.json`;
